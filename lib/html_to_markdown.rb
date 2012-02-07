9b458a0b3a87eb601809e7eb7f2b77fe46b630a3
David S <soulnafein@gmail.com>
2012-02-07 16:38:15 +0000

    Added an acceptance test for the library

--- lib/html_to_markdown.rb
+++ lib/html_to_markdown.rb
@@ -2,4 +2,7 @@ require "html_to_markdown/version"
 
 module HtmlToMarkdown
   # Your code goes here...
+  def self.parse(html)
+    ""
+  end
 end
--- tests/html_to_markdown/acceptance_tests.rb
+++ tests/html_to_markdown/acceptance_tests.rb
@@ -2,10 +2,16 @@ require_relative '../tests_setup.rb'
 
 module HtmlToMarkdown
   class AcceptanceTests < MiniTest::Unit::TestCase
-    def test_that_asserts_work
-      puts "Running"
-      name = 'David'
-      assert_equal 'David', name
+    def test_that_gem_can_convert_a_complex_html_document_to_markdown
+      source_html = load_fixture('introduction_to_markdown.html')
+      expected_markdown = load_fixture('introduction_to_markdown.markdown')
+      assert_equal expected_markdown, HtmlToMarkdown.parse(source_html)
+    end
+
+    private 
+    def load_fixture(fixture)
+      file_path = File.join(File.dirname(__FILE__), '../fixtures', fixture)
+      File.read(file_path)
     end
   end
 end
