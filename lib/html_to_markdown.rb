103e29699dd4ca90c05ad920cb053ffab5ef50dc
Federico Rebora <ufinii@gmail.com>
2012-02-07 19:23:07 +0100

    Two tests passing

--- lib/html_to_markdown.rb
+++ lib/html_to_markdown.rb
@@ -1,8 +1,18 @@
 require "html_to_markdown/version"
+require "nokogiri"
 
 module HtmlToMarkdown
   # Your code goes here...
-  def self.parse(html)
-    ""
+  def self.parse(html_document)
+    doc = Nokogiri::HTML(html_document)
+    output = ""
+    doc.xpath('//h1').each do |node|
+      output += "#{node.text}\n" + ("=" * node.text.length) + "\n\n"
+    end
+    doc.xpath('//h2').each do |node|
+      output += "#{node.text}\n" + ("-" * node.text.length) + "\n\n"
+    end
+
+    output
   end
 end
