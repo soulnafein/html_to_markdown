faa92262a1616be43d06165a5f24713964a1d840
Federico Rebora <ufinii@gmail.com>
2012-02-08 14:10:06 +0100

    Better use of Hash, using default value instead of checking for nil

--- lib/node_converter_bindings.rb
+++ lib/node_converter_bindings.rb
@@ -2,13 +2,17 @@ require "node_converters"
 
 module HtmlToMarkdown
   module NodeConverterBindings
-  	@@Converters = {"h1" => HeaderOneConverter.new, "h2" => HeaderTwoConverter.new, "text" => TextConverter.new, "p" => ParagraphConverter.new, "strong" => StrongConverter.new}
-  	@@NoMatchConverter = NoMatchConverter.new
+
+  	@@Converters = { "h1" => HeaderOneConverter.new,
+	                 "h2" => HeaderTwoConverter.new,
+	                 "text" => TextConverter.new,
+	                 "p" => ParagraphConverter.new,
+	                 "strong" => StrongConverter.new}
+	
+	@@Converters.default = NoMatchConverter.new
     
     def self.get_converter_for(node)
-      converter = @@Converters[node.name]
-      converter = @@NoMatchConverter if converter == nil
-      converter
+      @@Converters[node.name]
     end
   end
 end
