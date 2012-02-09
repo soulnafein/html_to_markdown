157fa03d49d07eecc840a193bb0d5978673eb7f9
Federico Rebora <ufinii@gmail.com>
2012-02-09 20:21:37 +0100

    added unordered lists

---
+++ tests/html_to_markdown/ordered_and_unordered_lists_tests.rb
@@ -0,0 +1,27 @@
+module HtmlToMarkdown
+  class OrderedAndUnorderedListsTests < MiniTest::Unit::TestCase
+    def test_unordered_list_with_one_element
+      html = "<ul><li>eggs</li></ul>"
+
+      markdown = "*  eggs\n"
+
+      assert_equal markdown, HtmlToMarkdown.parse(html)
+    end
+
+    def test_unordered_list_with_two_elements
+      html = "<ul><li>eggs</li><li>bacon</li></ul>"
+
+      markdown = "*  eggs\n*  bacon\n"
+
+      assert_equal markdown, HtmlToMarkdown.parse(html)
+    end
+
+    def test_unordered_list_with_two_elements_with_inner_html
+      html = "<ul><li><strong>eggs</strong></li><li><em>bacon</em></li></ul>"
+
+      markdown = "*  **eggs**\n*  _bacon_\n"
+
+      assert_equal markdown, HtmlToMarkdown.parse(html)
+    end
+  end
+end
